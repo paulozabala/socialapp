69b6fd34c14f5dded625e3d4f555b105bab6822b
paulozabala <balticst3ch@gmail.com>
2022-07-08 02:22:12 -0500

    dotenv added

--- server/app.js
+++ server/app.js
@@ -4,13 +4,16 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const dotenv = require ('dotenv');
 
 //setting listening port
 app.set('port',process.env.PORT || 3000);
 
+//dotenv config
+dotenv.config();
+
 //DB Connection
-const dir = 'mongodb+srv://socialite:socialinput@cluster0.mjixm.mongodb.net/?retryWrites=true&w=majority'; 
+const dir = process.env.DATABASE;
 const options = {useNewUrlParser:true,useUnifiedTopology:true};
 mongoose.connect(dir,options)
 	.then(()=>{
