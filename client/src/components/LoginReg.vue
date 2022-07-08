e0f199031160eab766ea9c5f49b6829d708034f9
paulozabala <balticst3ch@gmail.com>
2022-07-07 22:15:07 -0500

    Login preparado

--- client/src/components/LoginReg.vue
+++ client/src/components/LoginReg.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-container>
 		<v-row class="text-center pa-8" >
-			<v-col  md="6" sm="6" xs="12" align='center' >
+			<v-col  md="6" sm="5" xs="12" align='center' >
 				<v-img
 					src="../assets/community.svg" 
 					alt="community"
@@ -11,51 +11,32 @@
 				/>  <!--contain   causes picture not to be cropped -->
 			</v-col>
 			
-			<v-col  md="5" sm="5" xs="12" class="pa-10" offset="1" offset-xl="0">
-				<span class="text-h4 text-sm-h4 font-weight-black"> Login</span>
+			<v-col  md="5" sm="6" xs="12" class="pa-10" offset="1" offset-xl="0" >
+				<span class="text-h4 text-sm-h4 font-weight-black">Login</span>
 				<form class="">
 					<v-text-field
-						v-model="name"
+						v-model="userName"
 						:error-messages="nameErrors"
 						:counter="10"
-						label="Name"
+						label="Nombre de usuario"
 						required
-						@input="$v.name.$touch()"
-						@blur="$v.name.$touch()"
-						></v-text-field>
+						@input="$v.userName.$touch()"
+						@blur="$v.userName.$touch()"
+					></v-text-field>	
 					<v-text-field
-						v-model="email"
-						:error-messages="emailErrors"
-						label="E-mail"
+						type="password"
+						v-model="password"
+						:error-messages="passErrors"
+						label="Password"
 						required
-						@input="$v.email.$touch()"
-						@blur="$v.email.$touch()"
+						@input="$v.password.$touch()"
+						@blur="$v.password.$touch()"
 					></v-text-field>
-					<v-select
-						v-model="select"
-						:items="items"
-						:error-messages="selectErrors"
-						label="Item"
-						required
-						@change="$v.select.$touch()"
-						@blur="$v.select.$touch()"
-					></v-select>
-					<v-checkbox
-						v-model="checkbox"
-						:error-messages="checkboxErrors"
-						label="Do you agree?"
-						required
-						@change="$v.checkbox.$touch()"
-						@blur="$v.checkbox.$touch()"
-					></v-checkbox>
 					<v-btn
-						class="mr-4"
+						class="mt-4"
 						@click="submit"
 					>
-						submit
-					</v-btn>
-					<v-btn @click="clear">
-						clear
+						Ingresar
 					</v-btn>
 				</form>
 			</v-col>
@@ -65,39 +46,41 @@
 
 <script>
 import { validationMixin } from 'vuelidate'
-import { required, maxLength, email } from 'vuelidate/lib/validators'
-
+import { required, maxLength } from 'vuelidate/lib/validators'
+//import { required, maxLength, email } from 'vuelidate/lib/validators'
   export default {
 	name: 'LoginReg',
 	
 	mixins: [validationMixin],
 
 	validations: {
-		name: { required, maxLength: maxLength(10) },
-		email: { required, email },
+		userName: { required, maxLength: maxLength(8) },
+		password: { required, maxLength: maxLength(8) },
+/*		email: { required, email },
 		select: { required },
 		checkbox: {
 			checked (val) {
 			return val
 			},
-		},
+		},*/
 		},
 
 	data: () => ({
-		name: '',
+
+		userName: '',
+		password:'',
 		email: '',
 		select: null,
-		items: [
-			'Item 1',
-			'Item 2',
-			'Item 3',
-			'Item 4',
+		sexo: [
+			'Hombre',
+			'Mujer',
+			'Prefiero  No  Decirlo',
 		],
 		checkbox: false,
 	}),
 
 	computed: {
-		checkboxErrors () {
+/*		checkboxErrors () {
 			const errors = []
 			if (!this.$v.checkbox.$dirty) return errors
 			!this.$v.checkbox.checked && errors.push('You must agree to continue!')
@@ -106,35 +89,43 @@ import { required, maxLength, email } from 'vuelidate/lib/validators'
 		selectErrors () {
 			const errors = []
 			if (!this.$v.select.$dirty) return errors
-			!this.$v.select.required && errors.push('Item is required')
+			!this.$v.select.required && errors.push('Sexo es requerido.')
 			return errors
-		},
+		},*/
 		nameErrors () {
 			const errors = []
-			if (!this.$v.name.$dirty) return errors
-			!this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-			!this.$v.name.required && errors.push('Name is required.')
+			if (!this.$v.userName.$dirty) return errors
+			!this.$v.userName.maxLength && errors.push('Nombre  debe tener como maximot 8 caracteres')
+			!this.$v.userName.required && errors.push('userName es requerido.')
 			return errors
 		},
-		emailErrors () {
+		passErrors () {
 			const errors = []
-			if (!this.$v.email.$dirty) return errors
-			!this.$v.email.email && errors.push('Must be valid e-mail')
-			!this.$v.email.required && errors.push('E-mail is required')
+			if (!this.$v.password.$dirty) return errors
+			!this.$v.password.required && errors.push('Password es requerido.')
 			return errors
 		},
+//		emailErrors () {
+//			const errors = []
+//			if (!this.$v.email.$dirty) return errors
+//			!this.$v.email.email && errors.push('Must be valid e-mail')
+//			!this.$v.email.required && errors.push('E-mail is required')
+//			return errors
+//		},
 	},
 
 	methods: {
 		submit () {
 			this.$v.$touch()
+			
 			},
 		clear () {
 			this.$v.$reset()
-			this.name = ''
-			this.email = ''
+			this.userName = ''
+			this.password = ''
+/*			this.email = ''
 			this.select = null
-			this.checkbox = false
+			this.checkbox = false*/
 		},
 	},
 }
