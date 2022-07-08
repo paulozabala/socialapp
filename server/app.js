774f6d3affa7508819087354887f462c6ba2b284
paulozabala <balticst3ch@gmail.com>
2022-07-08 02:01:17 -0500

    cors,mongoose added app listening on port and connection with DB ok

--- server/app.js
+++ server/app.js
@@ -1,9 +1,27 @@
+//importando modulos
 const express =  require('express');
 const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-app.set('port',3000);
+
+//setting listening port
+app.set('port',process.env.PORT || 3000);
+
+//DB Connection
+const dir = 'mongodb+srv://socialite:socialinput@cluster0.mjixm.mongodb.net/?retryWrites=true&w=majority'; 
+const options = {useNewUrlParser:true,useUnifiedTopology:true};
+mongoose.connect(dir,options)
+	.then(()=>{
+		console.log("Conectado a la base de datos");
+	})
+	.catch((error)=>{
+		console.log('error al conectarse a MongoDB: ',error);
+	});
 
 
+//Set home route
 app.get("/",function(req,res){
 	res.send("running")
 });
