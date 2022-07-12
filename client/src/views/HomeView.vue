9544944cc38c5b4b968ec0565487de1e069bc883
paulozabala <balticst3ch@gmail.com>
2022-07-12 08:51:18 -0500

    Home additions

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,22 +1,25 @@
 <template>
-	<container>
+	<container class="d-flex align-center flex-column">
 		<row>
 			<Vote_Rank />
 		</row>
 		<row>
-			<v-card>
-				<v-col>
+			<v-card width="450">
+				<v-col cols="2">
 					<v-avatar>
-					
+						<v-img
+							src="../assets/avatar.jpeg"
+						/>
 					</v-avatar>
 				</v-col>
-
-				<v-text-field
-					v-model="first"
-					label="First Name"
-					outlined
-					rounded
+				<v-col  cols="10">
+					<v-text-field
+						v-model="first"
+						label="First Name"
+						outlined
+						rounded
 				></v-text-field>
+				</v-col>
 			</v-card>
 		</row>
 	
